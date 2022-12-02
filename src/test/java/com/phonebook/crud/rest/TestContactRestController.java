6ae2d239ec302c1851908f2ee80ed83bccc99941
tsoodula <tarun.soodula@capgemini.com>
2022-12-02 15:16:25 +0530

    Added test methods for getAllContacts and saveContact removed slf4j dependency from pom.xml

--- src/test/java/com/phonebook/crud/rest/TestContactRestController.java
+++ src/test/java/com/phonebook/crud/rest/TestContactRestController.java
@@ -1,6 +1,9 @@
 package com.phonebook.crud.rest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -27,7 +30,7 @@ public class TestContactRestController {
 	private MockMvc mockMvc;
 	
 	@Test
-	public void testSaveContact() throws Exception {
+	public void testSaveContact_01() throws Exception {
 		
 		// defining behaviour for the mock object.
 		Mockito.when(contactService.createContact(Mockito.any())).thenReturn(true);
@@ -58,6 +61,84 @@ public class TestContactRestController {
 		// making the assertEquals
 		assertEquals(201, status);
 		
+	}
+	
+	@Test
+	public void testSaveContact_02() throws Exception {
+		
+		// defining behaviour for the mock object.
+		Mockito.when(contactService.createContact(Mockito.any())).thenReturn(false);
+		
+		Contact c = new Contact(101, "Tarun", "89898989", "tarun@gmail.com");
+		
+		String json = null;
+		
+		// converting java obj to josn. using mapper
+		ObjectMapper mapper = new ObjectMapper();
+		json = mapper.writeValueAsString(c);
+		
+		// creating the post request.
+		MockHttpServletRequestBuilder reqBuilder = 
+				MockMvcRequestBuilders.post("/api/contacts")
+									.contentType("application/json")
+									.content(json);
+		
+		// performing the rest call.
+		MvcResult mvcResult = mockMvc.perform(reqBuilder).andReturn();
+		
+		// getting the response from performed api call
+		MockHttpServletResponse response = mvcResult.getResponse();
+		
+		// getting the status code from response
+		int status = response.getStatus();
+		
+		// making the assertEquals
+		assertEquals(500, status);
+		
+	}
+	
+	@Test
+	public void testSaveContact_03() throws Exception {
+		
+		// defining behaviour for the mock object.
+		Mockito.when(contactService.createContact(Mockito.any())).thenThrow(RuntimeException.class);
+		
+		Contact c = new Contact(101, "Tarun", "89898989", "tarun@gmail.com");
+		
+		String json = null;
+		
+		// converting java obj to josn. using mapper
+		ObjectMapper mapper = new ObjectMapper();
+		json = mapper.writeValueAsString(c);
+		
+		// creating the post request.
+		MockHttpServletRequestBuilder reqBuilder = 
+				MockMvcRequestBuilders.post("/api/contacts")
+									.contentType("application/json")
+									.content(json);
+		
+		// performing the rest call.
+		MvcResult mvcResult = mockMvc.perform(reqBuilder).andReturn();
+		
+		// getting the response from performed api call
+		MockHttpServletResponse response = mvcResult.getResponse();
+		
+		// getting the status code from response
+		int status = response.getStatus();
+		
+		// making the assertEquals
+		assertEquals(500, status);
+		
+	}
+	
+	@Test
+	public void testFindAllContacts() throws Exception {
+		when(contactService.getAllContacts()).thenReturn(Collections.emptyList());
+		MockHttpServletRequestBuilder reqBuilder = MockMvcRequestBuilders.get("/api/contacts");
+		MvcResult mvcResult = mockMvc.perform(reqBuilder).andReturn();
+		MockHttpServletResponse response = mvcResult.getResponse();
+		int status = response.getStatus();
+		assertEquals(200, status);
 	}
 	
 
